042156d70361795a3593bfe2dd0d3eca6ea09b40
Hannes <hasnep@gmail.com>
2020-04-05 18:16:15 +0100

    Add rollup uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: "src/index.ts",
   output: { file: "dist/index.js" },
-  plugins: [json({ compact: true }), typescript({ tsconfig: "tsconfig.json" })],
+  plugins: [json({ compact: true }), typescript({ tsconfig: "tsconfig.json" }), uglify()],
   treeshake: false,
 };
